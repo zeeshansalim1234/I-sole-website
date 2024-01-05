0dd40a55ddc417e5ce51d0c83b37a34c103e3b18
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-01-05 03:05:50 -0700

    Dr. adde

--- myapp/src/FeedbackMessage.jsx
+++ myapp/src/FeedbackMessage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const FeedbackMessage = ({ date, message, index, doctorName }) => {
+  // Get the userRole and currName from local storage
+  const userRole = localStorage.getItem('userRole');
+  const currUsername = localStorage.getItem('curr_username'); // Replace with your actual storage key
+
+  // Define the doctor's name based on the userRole and currName
+  let doctorDisplayName = doctorName;
+
+  if (userRole === 'Doctor' && doctorName === currUsername) {
+    doctorDisplayName = `Dr. ${doctorName}`;
+  }
+
   return (
     <div className="feedback-message">
       <div className="message-header">
         <div className="doctor-info">
           <span className="doctor-icon">👤</span> {/* Replace with an actual image or icon */}
-          <span className="doctor-name">{doctorName}</span>
+          <span className="doctor-name">{doctorDisplayName}</span>
         </div>
         <div className="message-details">
           <span className="message-index">{index}</span>
