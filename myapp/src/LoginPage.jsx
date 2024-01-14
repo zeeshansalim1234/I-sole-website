d783cc64eb327e76d8b2f219e1d5fd96b21c362d
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-01-13 22:44:25 -0700

    detailed error handling on sign up page

--- myapp/src/LoginPage.jsx
+++ myapp/src/LoginPage.jsx
@@ -41,9 +41,22 @@ const LoginPage = () => {
       }
       
     } catch (error) {
-      // Handle API request errors
-      console.error('Error signing in:', error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error(error.response.data);
+        console.error(error.response.status);
+        console.error(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error', error.message);
+      }
+      console.error(error.config);
     }
+    
   };
 
   return (
