06ddef9fc5640a2e386f0b07ba6fe95dd2e3a827
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-01-13 20:05:41 -0700

    trial router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import FeedbackPage from './FeedbackPage';
 import SignupPage from './SignupPage';
 import LoginPage from './LoginPage';
 import Settings from './Settings';
 import AnalyticsPage from './AnalyticsPage';
 
-
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Updated route for the login page */}
         <Route path="/signup" element={<SignupPage />} />
-
-        {/* Updated route for the login page */}
         <Route path="/login" element={<LoginPage />} />
-        
-        {/* Updated route for the feedback page */}
         <Route path="/feedback" element={<FeedbackPage />} />
-
         <Route path="/analytics" element={<AnalyticsPage />} />
-
-        {/* Updated route for the feedback page */}
         <Route path="/settings" element={<Settings />} />
-
-        {/* Default route in case no other route matches */}
         <Route path="*" element={<SignupPage />} />
       </Routes>
     </Router>
