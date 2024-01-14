438be7152c1c134b9c328b2eedc4d8564f49486b
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-01-13 20:06:41 -0700

    revert

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FeedbackPage from './FeedbackPage';
 import SignupPage from './SignupPage';
 import LoginPage from './LoginPage';
 import Settings from './Settings';
 import AnalyticsPage from './AnalyticsPage';
 
+
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Updated route for the login page */}
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* Updated route for the login page */}
         <Route path="/login" element={<LoginPage />} />
+        
+        {/* Updated route for the feedback page */}
         <Route path="/feedback" element={<FeedbackPage />} />
+
         <Route path="/analytics" element={<AnalyticsPage />} />
+
+        {/* Updated route for the feedback page */}
         <Route path="/settings" element={<Settings />} />
+
+        {/* Default route in case no other route matches */}
         <Route path="*" element={<SignupPage />} />
       </Routes>
     </Router>
