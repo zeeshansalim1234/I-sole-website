cae6bde0996c5b26854db1b9e85fcb0faff63984
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-04-03 09:15:48 -0600

    adding garbage in firebase api key

--- myapp/src/firebase.js
+++ myapp/src/firebase.js
@@ -3,7 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+    apiKey: "AIzaSyC6sMzy1QQvaorLStq67a7ApL7zZTSd27waaaaaaaaaaa",
+    authDomain: "i-sole-new.firebaseapp.com",
+    projectId: "i-sole-new",
+    storageBucket: "i-sole-new.appspot.com",
+    messagingSenderId: "458816858957",
+    appId: "1:458816858957:web:046428876f5ebc0e2fa504",
+    measurementId: "G-226B2B9EX4"
+  };
   
 
 // Initialize Firebase
