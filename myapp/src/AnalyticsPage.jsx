9ac5b5badfbdd009f209cc920b7f487279a0e738
Zeeshan Chougle <zeeshan.chougle@ucalgary.ca>
2024-04-03 18:25:56 -0600

    button orientation updated

--- myapp/src/AnalyticsPage.jsx
+++ myapp/src/AnalyticsPage.jsx
@@ -339,27 +339,27 @@ function Analytics() {
                       P1
                     </button>
                     <button
-                      className={`regionButton regionButton2 ${footRegion === 'p2' ? 'selectedToggle' : ''}`}
+                      className={`regionButton regionButton2 ${footRegion === 'p5' ? 'selectedToggle' : ''}`}
                       onClick={() => handleRegionButtonClick('p2')}>
                       P2
                     </button>
                     <button
-                      className={`regionButton regionButton3 ${footRegion === 'p3' ? 'selectedToggle' : ''}`}
+                      className={`regionButton regionButton3 ${footRegion === 'p6' ? 'selectedToggle' : ''}`}
                       onClick={() => handleRegionButtonClick('p3')}>
                       P3
                     </button>
                     <button
-                      className={`regionButton regionButton4 ${footRegion === 'p4' ? 'selectedToggle' : ''}`}
+                      className={`regionButton regionButton4 ${footRegion === 'p2' ? 'selectedToggle' : ''}`}
                       onClick={() => handleRegionButtonClick('p4')}>
                       P4
                     </button>
                     <button
-                      className={`regionButton regionButton5 ${footRegion === 'p5' ? 'selectedToggle' : ''}`}
+                      className={`regionButton regionButton5 ${footRegion === 'p3' ? 'selectedToggle' : ''}`}
                       onClick={() => handleRegionButtonClick('p5')}>
                       P5
                     </button>
                     <button
-                      className={`regionButton regionButton6 ${footRegion === 'p6' ? 'selectedToggle' : ''}`}
+                      className={`regionButton regionButton6 ${footRegion === 'p4' ? 'selectedToggle' : ''}`}
                       onClick={() => handleRegionButtonClick('p6')}>
                       P6
                     </button>
